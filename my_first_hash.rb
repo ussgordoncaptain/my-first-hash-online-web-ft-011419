c98058604df6c3d8242a05e254f2f05602171074
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-20 01:30:04 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,12 +36,12 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   
-
+  shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun poweder"] = 4
 
   # return the shipping_manifest hash below
-
+  return shipping_manifest
 end
